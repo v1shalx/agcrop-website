1f66455eaf3838ad87fd7930efbbb3593e3e51d3
vishal mahajan <mahajanxvishal@gmail.com>
2023-12-23 12:25:13 +0530

    made it responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,34 +1,3 @@
-// import React from "react";
-// import "./About.styles.css";
-// import pose from "../../components/About/images/pose.svg";
-
-// function About() {
-//   return (
-//     <div className="About">
-//       <div className="About-left-side">
-//         <img className="about-img-1" alt="About" src={pose} />
-        
-//       </div>
-//       <div className="About-right-side">
-//         <h3 className="About-heading">About <span>Us</span></h3>
-//         <p className="About-para">
-//           AG Corporation is serving the market proudly for the last 15 years. We
-//           are one of the prominent manufacturers and suppliers of a wide range
-//           of Hydropneumatic control panels, Fire fighting systems, and Electric
-//           control panels. AG Corporation is CPRI approved and manufactured in
-//           compliance with the industry-set norms and regulations. We have
-//           well-equipped and have large production and service capacity. We have
-//           been appreciated by many top organizations for the precise requirement
-//           of our customers in the most efficient manner
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default About;
-
-
 import React from 'react';
 import "./About.styles.css";
 import pose from "../../components/About/images/pose.svg";
@@ -37,15 +6,17 @@ const AboutSection = () => {
   return (
     <div className="About" id="about">
       <div className="About-left-side">
-      <img className="about-img-1" alt="About" src={pose} />
-        
-
+        <div className="centered">
+          <img className="about-img-1" alt="About" src={pose} />
+        </div>
       </div>
       <div className="About-right-side">
-        <h3 className="About-heading">About <span>Us</span></h3>
-        <p className="About-para">
-          AG Corporation is serving the market proudly for the last 15 years. We are one of the prominent manufacturers and suppliers of a wide range of Hydropneumatic control panels, Fire fighting systems, and Electric control panels. AG Corporation is CPRI approved and manufactured in compliance with the industry-set norms and regulations. We have well-equipped and have a large production and service capacity. We have been appreciated by many top organizations for the precise requirement of our customers in the most efficient manner
-        </p>
+        <div className="centered">
+          <h3 className="About-heading">About <span>Us</span></h3>
+          <p className="About-para">
+            AG Corporation is serving the market proudly for the last 15 years. We are one of the prominent manufacturers and suppliers of a wide range of Hydropneumatic control panels, Fire fighting systems, and Electric control panels. AG Corporation is CPRI approved and manufactured in compliance with the industry-set norms and regulations. We have well-equipped and have a large production and service capacity. We have been appreciated by many top organizations for the precise requirement of our customers in the most efficient manner
+          </p>
+        </div>
       </div>
     </div>
   );
